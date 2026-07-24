33ccd8410ba81b5cb9cb5891c72b6678f1681b6a
Husnain56 <husnainhere.2019.5@gmail.com>
2026-07-24 18:52:55 +0500

    feat: implement RetrieveRelevantContent function in retriever to return contents of top N chunks in points

--- internal/retriever/retriever.go
+++ internal/retriever/retriever.go
@@ -3,10 +3,36 @@ package retriever
 import (
 	"context"
 
+	"github.com/Husnain56/rag-from-scratch/internal/embedder"
 	"github.com/qdrant/go-client/qdrant"
 )
 
-func QueryChunks(query string, limit uint64, client *qdrant.Client, collectionName string, embedding []float32) ([]*qdrant.ScoredPoint, error) {
+func RetrieveRelevantContent(query string, limit uint64, client *qdrant.Client, collectionName string) ([]string, error) {
+
+	embedding, err := embedder.CreateSingleTextEmbedding(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	vec := make([]float32, len(embedding))
+	for j, v := range embedding {
+		vec[j] = float32(v)
+	}
+
+	points, err := QueryPoints(query, limit, client, collectionName, vec)
+	if err != nil {
+		return nil, err
+	}
+
+	content := make([]string, len(points))
+	for _, point := range points {
+		content = append(content, point.Payload["content"].GetStringValue())
+	}
+	return content, nil
+}
+
+func QueryPoints(query string, limit uint64, client *qdrant.Client, collectionName string, embedding []float32) ([]*qdrant.ScoredPoint, error) {
 
 	points, err := client.Query(context.Background(), &qdrant.QueryPoints{
 		CollectionName: collectionName,
